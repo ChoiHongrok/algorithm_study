e7f13e097ffb4470e200333fa20571d34e94b911
Choi Hongrok <zxcv5317@gmail.com>
2023-09-05 22:53:26 +0900

    14500 done

--- BAEKJOON/14500_테트로미노.py
+++ BAEKJOON/14500_테트로미노.py
@@ -3,10 +3,82 @@ https://www.acmicpc.net/problem/14500
 '''
 
 import sys
-
-N, M = map(int, sys.stdin.readline().strip().split())
-print(1)
+input = sys.stdin.readline
+N, M = map(int, input().strip().split())
 mat = []
 for i in range(N):
-    row = sys.stdin.readline().strip().split()
+    row = list(map(int, input().strip().split()))
     mat.append(row)
+
+
+
+min_i, max_i = 0, N-1
+min_j, max_j = 0, M-1
+ 
+def dfs(i, j, size, cum_sum, log):
+    if visited[i][j] == True:
+        return 0
+    visited[i][j] = True
+    cum_sum += mat[i][j]
+    log += f'({mat[i][j]}) '
+    if size == 4:
+        # print(f'{log}, {cum_sum}')
+        # [print(row) for row in visited]
+        visited[i][j] = False
+        return cum_sum
+    
+    max_ret_sum = 0
+    if i > min_i:
+        ret_sum = dfs(i-1, j, size+1, cum_sum, log+'상')
+        max_ret_sum = max(max_ret_sum, ret_sum)
+    if i < max_i:
+        ret_sum = dfs(i+1, j, size+1, cum_sum, log+'하')
+        max_ret_sum = max(max_ret_sum, ret_sum)
+    if j > min_j:
+        ret_sum = dfs(i, j-1, size+1, cum_sum, log+'좌')
+        max_ret_sum = max(max_ret_sum, ret_sum)
+    if j < max_j:
+        ret_sum = dfs(i, j+1, size+1, cum_sum, log+'우')
+        max_ret_sum = max(max_ret_sum, ret_sum)
+    visited[i][j] = False
+    return max_ret_sum
+
+
+def cacl_o(i, j):
+    sides = [0, 0, 0, 0]
+    cnt = 0
+    if i > min_i:
+        sides[0] = mat[i-1][j]
+        cnt += 1
+    if i < max_i:
+        sides[1] = mat[i+1][j]
+        cnt += 1
+    if j > min_j:
+        sides[2] = mat[i][j-1]
+        cnt += 1
+    if j < max_j:
+        sides[3] = mat[i][j+1]
+        cnt += 1
+    if cnt < 3:
+        return 0
+    max_val = 0
+    val_sum = sum(sides)
+    for k in range(4):
+        new_val = val_sum - sides[k]
+        max_val = max(new_val, max_val)
+        # print(f'{sides}, i: {i}, j:{j}, val:{new_val}')
+    return max_val + mat[i][j]
+    
+        
+max_sum = 0
+visited = [[False for j in range(M)] for i in range(N)]
+for i in range(N):
+    for j in range(M):
+        # print(f'>>>>>>i:{i}, j:{j}, max_sum: {max_sum}')
+        _sum = dfs(i, j, size=1, cum_sum=0, log='')
+        # print(f'_sum: {_sum}')
+        max_sum = max(max_sum, _sum)
+        _sum = cacl_o(i, j)
+        # print(f'_sum: {_sum}')
+        max_sum = max(max_sum, _sum)
+print(max_sum)
