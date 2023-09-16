06ba3f12664937a47e08e83543aa805d0be09041
ChoiHongrok <zxcv5317@naver.com>
2023-09-16 20:09:45 +0900

    15652 done

--- BAEKJOON/15652_N과M(4).py
+++ BAEKJOON/15652_N과M(4).py
@@ -1,3 +1,19 @@
 '''
 https://www.acmicpc.net/problem/15652
 '''
+
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().strip().split())
+
+def dfs(start, log, cnt):
+    if cnt == M:
+        print(log)
+    else:
+        for n in range(start, N+1):
+            dfs(n, log+f' {n}', cnt+1)
+
+for start in range(1, N+1):
+    dfs(start, f'{start}', 1)
